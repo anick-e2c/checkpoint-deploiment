b56203120eace4bbbf2da147ee6dc3f11c3de271
koueAnicet <anicetkoue@gmail.com>
2025-06-25 12:34:42 +0000

    mise à jour des dépendances

--- server/controllers/profile.js
+++ server/controllers/profile.js
@@ -4,7 +4,7 @@ const newProfile = (req, res, next) => {
 };
 // newProfile function to get profile data
 
-const getProfile=(req,res,next)=>{
+const getProfile=(req, res, next)=>{
 res.json({message: "get profile data"});
 }
 
